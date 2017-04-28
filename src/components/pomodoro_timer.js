837fcac6d7ec531fd6734bc0116f2d7c7548c97a
Peter Krevenets <peter.k.nets@gmail.com>
2017-04-28 14:14:44 +0300

    Add placeholder data & basic styling for PomodoroTimer component

--- src/components/pomodoro_timer.js
+++ src/components/pomodoro_timer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import '../../style/pomodoro_timer.css';
+
 export default class PomodoroTimer extends Component {
   render() {
+    const time = '25:00';
+
     return (
-      <div>
-        Pomodoro Main
+      <div className="timer">
+        {time}
       </div>
     );
   }
