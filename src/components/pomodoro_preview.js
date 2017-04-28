511471051bff36d8e8a0bf42b9d00d986c141cac
Peter Krevenets <peter.k.nets@gmail.com>
2017-04-28 14:06:22 +0300

    Populate PomodoroPreview component with placeholder data

--- src/components/pomodoro_preview.js
+++ src/components/pomodoro_preview.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 export default class PomodoroPreview extends Component {
   render() {
     return (
-      <div>
-        Pomodoro Preview
+      <div className="preview">
+        <div className="preview-session">
+          <h3>SESSION</h3>
+          <div>25:00</div>
+        </div>
+        <div className="preview-break">
+          <h3>BREAK</h3>
+          <div>5:00</div>
+        </div>
       </div>
     );
   }
