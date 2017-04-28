455ff95e86242b9fa5ffb8e09871dbe8e67a5fbb
Peter Krevenets <peter.k.nets@gmail.com>
2017-04-28 13:59:49 +0300

    Create PomodoroPreview component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import PomodoroPreview from './pomodoro_preview';
 import PomodoroTimer from './pomodoro_timer';
 import PomodoroButtons from './pomodoro_buttons';
 import PomodoroSettings from './pomodoro_settings';
@@ -8,6 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <PomodoroPreview />
         <PomodoroTimer />
         <PomodoroButtons />
         <PomodoroSettings />
---
+++ src/components/pomodoro_preview.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PomodoroPreview extends Component {
+  render() {
+    return (
+      <div>
+        Pomodoro Preview
+      </div>
+    );
+  }
+}
