31391848ba49c5cc570d6a3058c964643f62c283
Peter Krevenets <peter.k.nets@gmail.com>
2017-04-28 14:19:20 +0300

    Add placeholder data & styling for PomodoroButtons component

--- src/components/pomodoro_buttons.js
+++ src/components/pomodoro_buttons.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import '../../style/pomodoro_buttons.css';
+
 export default class PomodoroButtons extends Component {
   render() {
     return (
-      <div>
-        Pomodoro Buttons
+      <div className="buttons">
+        <button>START</button>
+        <button>NEXT</button>
+        <button>RESET</button>
       </div>
     );
   }
